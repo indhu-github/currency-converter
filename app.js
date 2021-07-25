94a3ebe636616888c9c0cd11dae5aeb6903d50ba
indhu-github <indhusworkmail@gmail.com>
2021-07-25 20:47:39 +0530

    click event

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const input = document.getElementById("rupees");
 const output = document.getElementById("output");
-
 const dollar = document.getElementById("dollar");
-//input.addEventListener("keyup", convertCurrency);
-dollar.addEventListener("click", convertCurrency);
 
-function convertCurrency(e) {
-  let usDollar = input.value / 64;
+const convertToDollar = function (prop) {
+  console.log(prop);
+  console.log(prop / 64);
+  return prop / 64;
+};
+
+//input.addEventListener("keyup", convertCurrency);
+
+const convertCurrency = function (currency) {
+  console.log(input.value);
+  const usDollar = currency(input.value);
+  console.log(usDollar);
   document.querySelector("#output").value = usDollar;
-}
+};
+dollar.addEventListener("click", convertCurrency(convertToDollar));
