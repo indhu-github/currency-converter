69a7dc13682b841ead502aaa7dfa82bc1eb7929b
indhu-github <indhusworkmail@gmail.com>
2021-07-25 15:26:19 +0530

    logic part

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const input = document.getElementById("rupees");
 const output = document.getElementById("output");
 
+input.addEventListener("keyup", convertCurrency);
 
-input.addEventListener("keyup",convertCurrency);
-
-function convertCurrency(){
-    
+function convertCurrency(e) {
+  let usDollar = e.target.value / 64;
+  console.log(usDollar);
+  document.querySelector("#output").value = usDollar;
+  console.log(output);
 }
