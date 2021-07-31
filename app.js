df547533a7088f1ea8b467e0496d5fe2ed9946e0
indhu-github <indhusworkmail@gmail.com>
2021-07-31 16:57:37 +0530

    functionality

--- app.js
+++ app.js
@@ -15,27 +15,27 @@ rupee.addEventListener("keyup", () => {
     document.querySelector("#output").value = rupee.value / 64;
   });
   euro.addEventListener("click", function () {
-    document.querySelector("#output").value = rupee.value / 32;
+    document.querySelector("#output").value = rupee.value * 0.011;
   });
   dinar.addEventListener("click", function () {
-    document.querySelector("#output").value = rupee.value / 32;
+    document.querySelector("#output").value = rupee.value * 0.004;
   });
   pound.addEventListener("click", function () {
-    document.querySelector("#output").value = rupee.value / 32;
+    document.querySelector("#output").value = rupee.value * 0.0097;
   });
   bitcoin.addEventListener("click", function () {
-    document.querySelector("#output").value = rupee.value / 32;
+    document.querySelector("#output").value = (rupee.value * 0.000032) / 100;
   });
   rubel.addEventListener("click", function () {
-    document.querySelector("#output").value = rupee.value / 12;
+    document.querySelector("#output").value = rupee.value * 0.98;
   });
   ausDollar.addEventListener("click", function () {
-    document.querySelector("#output").value = rupee.value / 32;
+    document.querySelector("#output").value = rupee.value * 0.018;
   });
   canDollar.addEventListener("click", function () {
-    document.querySelector("#output").value = rupee.value / 32;
+    document.querySelector("#output").value = rupee.value * 0.017;
   });
   yen.addEventListener("click", function () {
-    document.querySelector("#output").value = rupee.value / 32;
+    document.querySelector("#output").value = rupee.value * 1.47;
   });
 });
