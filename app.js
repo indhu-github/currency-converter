1a9d0c1f4045408748349ab2cafd5a5965f045cf
indhu-github <indhusworkmail@gmail.com>
2021-07-31 16:35:34 +0530

    functionality

--- app.js
+++ app.js
@@ -2,12 +2,40 @@ var rupee = document.getElementById("rupees");
 var output = document.getElementById("output");
 var dollar = document.getElementById("dollar");
 var euro = document.getElementById("euro");
+var dinar = document.getElementById("dinar");
+var pound = document.getElementById("pound");
+var bitcoin = document.getElementById("bitcoin");
+var rubel = document.getElementById("rubel");
+var ausDollar = document.getElementById("ausDollar");
+var canDollar = document.getElementById("canDollar");
+var yen = document.getElementById("yen");
 
 rupee.addEventListener("keyup", () => {
   dollar.addEventListener("click", function () {
     document.querySelector("#output").value = rupee.value / 64;
   });
   euro.addEventListener("click", function () {
+    document.querySelector("#output").value = rupee.value / 32;
+  });
+  dinar.addEventListener("click", function () {
+    document.querySelector("#output").value = rupee.value / 32;
+  });
+  pound.addEventListener("click", function () {
+    document.querySelector("#output").value = rupee.value / 32;
+  });
+  bitcoin.addEventListener("click", function () {
+    document.querySelector("#output").value = rupee.value / 32;
+  });
+  rubel.addEventListener("click", function () {
+    document.querySelector("#output").value = rupee.value / 12;
+  });
+  ausDollar.addEventListener("click", function () {
+    document.querySelector("#output").value = rupee.value / 32;
+  });
+  canDollar.addEventListener("click", function () {
+    document.querySelector("#output").value = rupee.value / 32;
+  });
+  yen.addEventListener("click", function () {
     document.querySelector("#output").value = rupee.value / 32;
   });
 });
